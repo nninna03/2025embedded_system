4c775303e74fd19898eddaf3c0638820b91bc927
nninna03 <117813477+nninna03@users.noreply.github.com>
2025-06-18 23:40:47 +0900

    Create main.c

---
+++ homework/led/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <ctype.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "led.h"
+
+int main(int argc, char** argv) {
+	
+    int fd = led_init();
+    
+    
+    printf("hihihihih\n");
+    while(1) {
+        for (int i = 0; i<=7; i++) {
+            ledOnOff(i,1);
+            printf("LED %d ON\n", i + 1);
+            sleep(1);
+        }
+
+        for (int i = 0; i<=7; i++) {
+            ledOnOff(i,0);
+            printf("LED %d OFF\n", i + 1);
+            sleep(1);
+        }
+        printf("bye \n");
+        ledLibExit();
+        return 0;
+    }
+}
